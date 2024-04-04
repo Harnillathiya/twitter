fe39ebe8aa5a01afc24fbd62e98c752e4e52aa80
harnillathiya <harnillathiya07@gmail.com>
2024-04-04 11:15:09 +0530

    setup redux

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { FaImage } from "react-icons/fa";
 import { MdOutlineGifBox } from "react-icons/md";
 import { BiPoll } from "react-icons/bi";
@@ -7,14 +8,29 @@ import { FaLocationDot } from "react-icons/fa6";
 import { GrSchedulePlay } from "react-icons/gr";
 import Avatar from 'react-avatar';
 import './Feed.css';
+import { addTweet } from '../redux/action';
 
 const CreatePost = () => {
+    const tweets = useSelector((state) => state.tweets)
+    console.log(tweets)
+    const dispatch = useDispatch()
+
+    const [tweet, setTweet] = useState('')
+
+    const saveTweet = () => {
+        const tweetPayload = {
+            id: 1,
+            tweet: tweet
+        };
+        dispatch(addTweet(tweetPayload))
+    }
+
     return (
         <div className='w-[100%]'>
-            <div className='mt-2'> 
+            <div className='mt-2'>
                 <div className=' info postmain flex items-center justify-between border-b border-gray-200'>
                     <div className='cursor-pointer hover:bg-gray-400 w-full text-center p-4'>
-                        <span>for you</span> 
+                        <span>for you</span>
                     </div>
                     <div className='cursor-pointer hover:bg-gray-400 w-full text-center p-4'>
                         <span>following</span>
@@ -24,26 +40,46 @@ const CreatePost = () => {
                 <div>
                     <div className='flex items-center p-4'>
                         <div >
-                        <Avatar src="https://pbs.twimg.com/profile_images/1661229397880492033/-1d0znir_400x400.jpg" size="40" round={true} />
+                            <Avatar src="https://pbs.twimg.com/profile_images/1661229397880492033/-1d0znir_400x400.jpg" size="40" round={true} />
                         </div>
-                        <input className='w-full outline-none border-none text-lg ml-3' type="text" placeholder='What is happing ?!'/>
+                        <input className='w-full outline-none border-none text-lg ml-3' type="text" placeholder='What is happing ?!' onChange={(e) => setTweet(e.target.value)} />
                     </div>
                     <div className='flex items-center justify-between p-4 border-b border-gray-300'>
                         <div className='flex justify-stretch my-1 icons'>
-                           <a href=""><FaImage/></a>
-                             <a href=""><MdOutlineGifBox/></a>
-                             <a href=""><BiPoll/></a>
-                             <a href=""><FiSmile/></a>
-                             <a href=""><GrSchedulePlay/></a>
-                             <a href=""><FaLocationDot/></a>
+                            <a href="a"><FaImage /></a>
+                            <a href="a"><MdOutlineGifBox /></a>
+                            <a href="a"><BiPoll /></a>
+                            <a href="a"><FiSmile /></a>
+                            <a href="a"><GrSchedulePlay /></a>
+                            <a href="a"><FaLocationDot /></a>
                         </div>
-                        <button className='bg-[#1D9Df0] text-lg text-white items-center px-4 py-1 border-none rounded-full'>post</button>
+                        <button className='bg-[#1D9Df0] text-lg text-white items-center px-4 py-1 border-none rounded-full' onClick={saveTweet}>post</button>
                     </div>
+                </div>
+            </div>
+
+            <div>
+                <div className='flex'>
+                    <div >
+                        <Avatar src="https://pbs.twimg.com/profile_images/1661229397880492033/-1d0znir_400x400.jpg" size="40" round={true} />
+                    </div>
+                    <div className='flex ml-4'>     
+                        <h2>Harsa Dash</h2>
+                        <p className='text-gray-400 text-sm'>@Harsa_Dash .1m</p>
+                    </div>
+
+
+                </div>
+                <div className=''>
+                    <p className='text-gray-400 text-sm'>I’m quitting YouTube</p>
+                </div>
+                <div>
+                    <input type="text" placeholder='tweet' />
                 </div>
             </div>
         </div>
     )
-    
+
 }
 
 export default CreatePost
