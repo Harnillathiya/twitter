7913cd1a9916c12393ea5f0d752aecea54e143f2
rahul dholakiya <dholakiyarahul0@gmail.com>
2024-04-17 11:07:30 +0530

    position changes

--- src/Explore/News.jsx
+++ src/Explore/News.jsx
@@ -5,23 +5,21 @@ import { forYou, name } from "../StaticData";
 const News = () => {
   return (
     <div>
-      <div>
-        <Explore />
-        {forYou.map((item, index) => (
-          <div key={index} className="foryou-mapdata">
-            <div>
-              <p>{item.nameOne}</p>
-              <p className="nametwo-text">{item.nameTwo}</p>
-              <p>
-                Trending With <span className="different-name">{name}</span>
-              </p>
-            </div>
-            <div>
-              <button>···</button>
-            </div>
+      <Explore />
+      {forYou.map((item, index) => (
+        <div key={index} className="foryou-mapdata">
+          <div>
+            <p>{item.nameOne}</p>
+            <p className="nametwo-text">{item.nameTwo}</p>
+            <p>
+              Trending With <span className="different-name">{name}</span>
+            </p>
           </div>
-        ))}
-      </div>
+          <div>
+            <p>···</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
