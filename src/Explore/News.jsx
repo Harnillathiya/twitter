1711942a2b6fe7315ac51b8705355b5f50663319
rahul dholakiya <dholakiyarahul0@gmail.com>
2024-04-22 18:01:10 +0530

    sportspage

--- src/Explore/News.jsx
+++ src/Explore/News.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import Explore from "../Page/Explore";
-import { forYou } from "../StaticData";
+import { news } from "../StaticData";
 
 const News = () => {
   return (
     <div>
       <Explore />
-      {forYou.map((item, index) => (
-        <div key={index} className="foryou-mapdata">
-          <div>
-            <p>{item.nameOne}</p>
-            <p className="nametwo-text">{item.nameTwo}</p>
-            <p>
-              Trending With{" "}
-              <span className="different-name">{item.nameThree}</span>
-            </p>
+      <div className="foryou-borderbox">
+        {news.map((item, index) => (
+          <div key={index} className="foryou-mapdata">
+            <div>
+              <p>{item.newsOne}</p>
+              <p className="nametwo-text">{item.newsTwo}</p>
+              <p>
+                Trending With{" "}
+                <span className="different-name">{item.newsThree}</span>
+              </p>
+            </div>
+            <div>
+              <p>···</p>
+            </div>
           </div>
-          <div>
-            <p>···</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/Explore/Sports.jsx
+++ src/Explore/Sports.jsx
@@ -6,21 +6,23 @@ const Sports = () => {
   return (
     <div>
       <Explore />
-      {forYou.map((item, index) => (
-        <div key={index} className="foryou-mapdata">
-          <div>
-            <p>{item.nameOne}</p>
-            <p className="nametwo-text">{item.nameTwo}</p>
-            <p>
-              Trending With{" "}
-              <span className="different-name">{item.nameThree}</span>
-            </p>
+      <div className="foryou-borderbox">
+        {forYou.map((item, index) => (
+          <div key={index} className="foryou-mapdata">
+            <div>
+              <p>{item.nameOne}</p>
+              <p className="nametwo-text">{item.nameTwo}</p>
+              <p>
+                Trending With{" "}
+                <span className="different-name">{item.nameThree}</span>
+              </p>
+            </div>
+            <div>
+              <p>···</p>
+            </div>
           </div>
-          <div>
-            <p>···</p>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
